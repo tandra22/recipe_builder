9949db64598cd436435ad179071f197e481ed8d5
Raghu Betina <raghu@firstdraft.com>
2019-06-10 22:01:25 +0000

    Generate items resource with draft:resource

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,63 @@
+class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+
+    render("item_templates/index.html.erb")
+  end
+
+  def show
+    @item = Item.find(params.fetch("id_to_display"))
+
+    render("item_templates/show.html.erb")
+  end
+
+  def new_form
+    @item = Item.new
+
+    render("item_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @item = Item.new
+
+    @item.name = params.fetch("name")
+    @item.category = params.fetch("category")
+
+    if @item.valid?
+      @item.save
+
+      redirect_back(:fallback_location => "/items", :notice => "Item created successfully.")
+    else
+      render("item_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @item = Item.find(params.fetch("prefill_with_id"))
+
+    render("item_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @item = Item.find(params.fetch("id_to_modify"))
+
+    @item.name = params.fetch("name")
+    @item.category = params.fetch("category")
+
+    if @item.valid?
+      @item.save
+
+      redirect_to("/items/#{@item.id}", :notice => "Item updated successfully.")
+    else
+      render("item_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @item = Item.find(params.fetch("id_to_remove"))
+
+    @item.destroy
+
+    redirect_to("/items", :notice => "Item deleted successfully.")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  # Routes for the Item resource:
+
+  # CREATE
+  get("/items/new", { :controller => "items", :action => "new_form" })
+  post("/create_item", { :controller => "items", :action => "create_row" })
+
+  # READ
+  get("/items", { :controller => "items", :action => "index" })
+  get("/items/:id_to_display", { :controller => "items", :action => "show" })
+
+  # UPDATE
+  get("/items/:prefill_with_id/edit", { :controller => "items", :action => "edit_form" })
+  post("/update_item/:id_to_modify", { :controller => "items", :action => "update_row" })
+
+  # DELETE
+  get("/delete_item/:id_to_remove", { :controller => "items", :action => "destroy_row" })
+
+  #------------------------------
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
