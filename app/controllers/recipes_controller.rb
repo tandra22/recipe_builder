031f77c2e05a432e9d43a76f5eec2a710ddb8a46
Raghu Betina <raghu@firstdraft.com>
2019-06-10 22:01:30 +0000

    Generate recipes resource with draft:resource

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,63 @@
+class RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+
+    render("recipe_templates/index.html.erb")
+  end
+
+  def show
+    @recipe = Recipe.find(params.fetch("id_to_display"))
+
+    render("recipe_templates/show.html.erb")
+  end
+
+  def new_form
+    @recipe = Recipe.new
+
+    render("recipe_templates/new_form.html.erb")
+  end
+
+  def create_row
+    @recipe = Recipe.new
+
+    @recipe.source_website = params.fetch("source_website")
+    @recipe.name = params.fetch("name")
+
+    if @recipe.valid?
+      @recipe.save
+
+      redirect_back(:fallback_location => "/recipes", :notice => "Recipe created successfully.")
+    else
+      render("recipe_templates/new_form_with_errors.html.erb")
+    end
+  end
+
+  def edit_form
+    @recipe = Recipe.find(params.fetch("prefill_with_id"))
+
+    render("recipe_templates/edit_form.html.erb")
+  end
+
+  def update_row
+    @recipe = Recipe.find(params.fetch("id_to_modify"))
+
+    @recipe.source_website = params.fetch("source_website")
+    @recipe.name = params.fetch("name")
+
+    if @recipe.valid?
+      @recipe.save
+
+      redirect_to("/recipes/#{@recipe.id}", :notice => "Recipe updated successfully.")
+    else
+      render("recipe_templates/edit_form_with_errors.html.erb")
+    end
+  end
+
+  def destroy_row
+    @recipe = Recipe.find(params.fetch("id_to_remove"))
+
+    @recipe.destroy
+
+    redirect_to("/recipes", :notice => "Recipe deleted successfully.")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
+  # Routes for the Recipe resource:
+
+  # CREATE
+  get("/recipes/new", { :controller => "recipes", :action => "new_form" })
+  post("/create_recipe", { :controller => "recipes", :action => "create_row" })
+
+  # READ
+  get("/recipes", { :controller => "recipes", :action => "index" })
+  get("/recipes/:id_to_display", { :controller => "recipes", :action => "show" })
+
+  # UPDATE
+  get("/recipes/:prefill_with_id/edit", { :controller => "recipes", :action => "edit_form" })
+  post("/update_recipe/:id_to_modify", { :controller => "recipes", :action => "update_row" })
+
+  # DELETE
+  get("/delete_recipe/:id_to_remove", { :controller => "recipes", :action => "destroy_row" })
+
+  #------------------------------
+
   # Routes for the Ingredient resource:
 
   # CREATE
