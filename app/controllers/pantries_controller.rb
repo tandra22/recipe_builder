50c341466d73242545b0f901bf496d8f1a7fb134
Raghu Betina <raghu@firstdraft.com>
2019-06-10 22:02:35 +0000

    Only pantry user can modify pantry

--- app/controllers/pantries_controller.rb
+++ app/controllers/pantries_controller.rb
@@ -1,4 +1,14 @@
 class PantriesController < ApplicationController
+  before_action :current_user_must_be_pantry_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_pantry_user
+    pantry = Pantry.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == pantry.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @pantries = Pantry.all
 
